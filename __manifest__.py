4cbc89bfe836b54555d72c5225425890e7dcded5
https://github.com/AbdouElnemr/ <abdouelnemr91@gmail.com>
2020-11-26 19:08:54 +0200

    add improvments to manifist

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "employee_car_request",
+    'name': "Employee Car Request",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,11 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Human Resource',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'hr', 'fleet'],
 
     # always loaded
     'data': [

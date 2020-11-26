0af7d3d0e42148b275b323c6ebd59052a2e7b919
https://github.com/AbdouElnemr/ <abdouelnemr91@gmail.com>
2020-11-26 23:35:15 +0200

    the third lecture

--- models/models.py
+++ models/models.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api
 class CarRequest(models.Model):
     _name = "car.request"
+    _inherit = "mail.thread"
     _description = "car desc"
 
     name = fields.Char(string="Request", required=True, )
@@ -10,3 +11,19 @@ class CarRequest(models.Model):
     date_to = fields.Datetime(string="End Date",)
     employee_id = fields.Many2one(comodel_name="hr.employee", string="Employee", required=True, )
     car_id = fields.Many2one(comodel_name="fleet.vehicle", string="Car", required=True, )
+    state = fields.Selection(string="", selection=[('draft', 'Draft'),('confirm', 'Confirm'),
+                                                   ('validate', 'Validate'), ('refuse', 'Refuse'),
+                                                   ('approved', 'Approved'), ], default="draft", track_visibility='onchange')
+
+    @api.multi
+    def confirm_request(self):
+        self.state = 'confirm'
+    @api.multi
+    def validate_request(self):
+        self.state = 'validate'
+    @api.multi
+    def refuse_request(self):
+        self.state = 'refuse'
+    @api.multi
+    def approve_request(self):
+        self.state = 'approved'
